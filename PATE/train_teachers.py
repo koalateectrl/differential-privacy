e36a506ac52f0aa17f9f9e3618f9ae5f834e6fb7
samwwong <sandymule@gmail.com>
2020-08-18 15:19:27 -0700

    fixes to more closely match paper

--- PATE/train_teachers.py
+++ PATE/train_teachers.py
@@ -6,6 +6,7 @@ Code that trains teachers for PATE model.
 import tensorflow as tf
 import numpy as np
 import time
+import math
 
 from absl import flags
 from absl import app
@@ -24,7 +25,6 @@ flags.DEFINE_integer('teacher_id', 0, 'ID of teacher being trained.')
 flags.DEFINE_integer('batch_size', 128, 'Batch size')
 
 flags.DEFINE_float('learning_rate', 0.05, 'Learning rate for training')
-flags.DEFINE_integer('epochs', 50, 'Number of epochs')
 
 FLAGS = flags.FLAGS
 
@@ -73,23 +73,27 @@ class MyModel(tf.keras.Model):
   def __init__(self):
     super(MyModel, self).__init__()
     self.conv1 = tf.keras.layers.Conv2D(
-        16, 8, strides=2, padding='same', activation='relu')
-    self.pool1 = tf.keras.layers.MaxPool2D(2, 1)
+        filters = 64, kernel_size = 5, strides = 1, padding='same', activation='relu')
+    self.pool1 = tf.keras.layers.MaxPool2D(pool_size = (3, 3), strides = (2, 2), padding = 'same')
     self.conv2 = tf.keras.layers.Conv2D(
-        32, 4, strides=2, padding='valid', activation='relu')
-    self.pool2 = tf.keras.layers.MaxPool2D(2, 1)
+        filters = 128, kernel_size = 5, strides = 1, padding='same', activation='relu')
+    self.pool2 = tf.keras.layers.MaxPool2D(pool_size = (3, 3), strides = (2, 2), padding = 'same')
     self.flat = tf.keras.layers.Flatten()
-    self.d1 = tf.keras.layers.Dense(32, activation='relu')
-    self.d2 = tf.keras.layers.Dense(10)
+    self.d1 = tf.keras.layers.Dense(384, activation = 'relu')
+    self.d2 = tf.keras.layers.Dense(192, activation = 'relu')
+    self.d3 = tf.keras.layers.Dense(10)
 
   def call(self, x):
     x = self.conv1(x)
     x = self.pool1(x)
+    x = tf.nn.local_response_normalization(x, depth_radius = 4, bias = 1, alpha = 0.001/9.0, beta = 0.75)
     x = self.conv2(x)
+    x = tf.nn.local_response_normalization(x, depth_radius = 4, bias = 1, alpha = 0.001/9.0, beta = 0.75)
     x = self.pool2(x)
     x = self.flat(x)
     x = self.d1(x)
     x = self.d2(x)
+    x = self.d3(x)
     return x
 
   def model(self):
@@ -145,8 +149,9 @@ def main(unused_argv):
   test_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(
       name='test_accuracy')
 
+  num_epochs = math.floor(FLAGS.max_steps / FLAGS.batch_size)
   # Training loop.
-  for epoch in range(1, FLAGS.epochs + 1):
+  for epoch in range(1, num_epochs + 1):
     start_time = time.time()
     # Train the model for one epoch.
     train_loss.reset_states()
